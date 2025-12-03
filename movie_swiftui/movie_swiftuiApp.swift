d8b2911df34b1a967589f66d74f0fbd25a3c37bb
abdelrahman <abdoarfat924@gmail.com>
2025-12-03 12:42:05 +0200

    Initial Commit

---
+++ movie_swiftui/movie_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_swiftuiApp.swift
+//  movie_swiftui
+//
+//  Created by Abdelrahman Arfat on 03/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct movie_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ movie_swiftuiTests/movie_swiftuiTests.swift
@@ -0,0 +1,16 @@
+//
+//  movie_swiftuiTests.swift
+//  movie_swiftuiTests
+//
+//  Created by Abdelrahman Arfat on 03/12/2025.
+//
+
+import Testing
+
+struct movie_swiftuiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
